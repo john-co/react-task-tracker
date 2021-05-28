9e0902460328d411ac369feb93b9ab02964f07f3
John Co <john.co@liferay.com>
2021-05-28 15:16:14 -0700

    Create Header and Button for tracker

---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+const tasks = []
+
+const Tasks = () => {
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3>{task.text}</h3>
+            ))}
+        </>
+    )
+}
+
+export default Tasks
